18a79530d2fd1bd9f8933186ebefa81524c0f4c1
Richard Cho <richardcho100@gmail.com>
2020-11-04 23:59:44 -0500

    Get canvas from DOM & create ball w/ props

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const rulesBtn = document.getElementById('rules-btn')
 const closeBtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
+const canvas = document.getElementById('canvas')
+
+// Create ball props
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+};
 
 rulesBtn.addEventListener('click', (e) => rules.classList.add(('show')))
 closeBtn.addEventListener('click', (e) => rules.classList.remove(('show')))
