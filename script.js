d49870bd8aa97e3aef2d397403bafca97cdbc1f0
Richard Cho <richardcho100@gmail.com>
2020-11-05 23:07:38 -0500

    Add function to draw ball on canvas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const rulesBtn = document.getElementById('rules-btn')
 const closeBtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
 const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
 // Create ball props
 const ball = {
@@ -11,7 +12,16 @@ const ball = {
     speed: 4,
     dx: 4,
     dy: -4
-};
+}
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
 
 rulesBtn.addEventListener('click', (e) => rules.classList.add(('show')))
 closeBtn.addEventListener('click', (e) => rules.classList.remove(('show')))
