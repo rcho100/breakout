976f01ebbae5b9ba8942a165165b92f8079011d3
Richard Cho <richardcho100@gmail.com>
2020-11-06 23:02:58 -0500

    Create paddle props & drawPaddle function

--- script.js
+++ script.js
@@ -14,10 +14,29 @@ const ball = {
     dy: -4
 }
 
+// Create paddle props
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0
+}
+
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath()
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+// Draw paddle on canvas
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
     ctx.fillStyle = '#0095dd'
     ctx.fill()
     ctx.closePath()
